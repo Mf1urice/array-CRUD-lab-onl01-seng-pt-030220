e58450c65fa318879d6a730a27b2e61010aa4b20
Maurice Falls <falls8019oct@gmail.com>
2020-03-15 03:08:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
- element_end_of_array = 
+ create_an_array = ["I", "am", "really", "learning"]
+ remove_element_from_end_of_array = create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
